5fbe4c7a4668140a41ebadba20e29ba026d3340e
unknown <tarun.cosmic@gmail.com>
2022-05-01 23:13:32 +0530

    added hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+
+    printf("Hello world");
+    return 0;
+}
